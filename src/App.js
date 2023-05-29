323d6689571a35d9ce053518d6f465db51a71ba9
Grishmita <grishmitapandey@gmail.com>
2023-05-29 06:26:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import InputText from "./Input";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App with React Hooks
+        (Click enter after you are completed with writing your task so that it add to ypur list)
+      </h1>
+      <InputText />
+    </div>
+  );
+}
+
+export default App;
